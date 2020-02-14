f427bb8c053a0670bbf05c7d43e91fa15d395532
Serina Tsang <serinat@uw.edu>
2020-02-13 23:02:10 -0800

    updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,33 @@
-USE employee_tracker_db;
+use employees;
 
-INSERT INTO department (name)
-VALUES ("Engineering"), ("Sales"), ("Finance");
-
-INSERT INTO role (title, salary, department_id)
+INSERT INTO department
+    (name)
 VALUES
-("Software Engineer", 100000, 1),
-("Aeronautical Engineer", 200000, 1),
-("Sales Manager", 80000, 2),
-("Salesperson", 60000, 2),
-("Accountant", 90000, 3),
-("Fiscal Specialist", 70000, 3);
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO role
+    (title, salary, department_id)
 VALUES
-("Harry", "Furusho", 1, 1),
-("Norman", "Lei", 1, 2),
-("Lily", "Chon", 2, 5),
-("Jasmine", "Wang", 3, 2),
-("Arnold", "Diep", 2, 5),
-("Chris", "Barcala", 3, 1);
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
